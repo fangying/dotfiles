67b589ad92f957f8a296e4a1d39dfb2819fad4ae
Ying Fang <fangying712@gmail.com>
2022-09-07 17:41:15 +0800

    wezterm: Add wezterm.lua
    Signed-off-by: Ying Fang <fangying712@gmail.com>

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,51 @@
+local wezterm = require("wezterm")
+
+local config = {
+    check_for_updates = false,
+    -- color_scheme = "Catppuccin Mocha",
+	color_scheme = "Tango (terminal.sexy)",
+    inactive_pane_hsb = {
+        hue = 1.0,
+        saturation = 1.0,
+        brightness = 1.0,
+    },
+    default_prog = { '/bin/zsh', '-l' },
+    font = wezterm.font_with_fallback { 'Hack Nerd Font Mono', 'SFMono Nerd Font'},
+    font_size = 17.0,
+    launch_menu = {},
+    leader = { key="a", mods="CTRL" },
+    disable_default_key_bindings = false,
+    keys = {
+        -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+        { key = "a", mods = "LEADER|CTRL",  action=wezterm.action{SendString="\x01"}},
+        { key = "-", mods = "LEADER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+        { key = "\\",mods = "LEADER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+        { key = "z", mods = "LEADER",       action="TogglePaneZoomState" },
+        { key = "c", mods = "LEADER",       action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
+        { key = "h", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Left"}},
+        { key = "j", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Down"}},
+        { key = "k", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Up"}},
+        { key = "l", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Right"}},
+        { key = "H", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
+        { key = "J", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
+        { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
+        { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
+        { key = "1", mods = "LEADER",       action=wezterm.action{ActivateTab=0}},
+        { key = "2", mods = "LEADER",       action=wezterm.action{ActivateTab=1}},
+        { key = "3", mods = "LEADER",       action=wezterm.action{ActivateTab=2}},
+        { key = "4", mods = "LEADER",       action=wezterm.action{ActivateTab=3}},
+        { key = "5", mods = "LEADER",       action=wezterm.action{ActivateTab=4}},
+        { key = "6", mods = "LEADER",       action=wezterm.action{ActivateTab=5}},
+        { key = "7", mods = "LEADER",       action=wezterm.action{ActivateTab=6}},
+        { key = "8", mods = "LEADER",       action=wezterm.action{ActivateTab=7}},
+        { key = "9", mods = "LEADER",       action=wezterm.action{ActivateTab=8}},
+        { key = "&", mods = "LEADER|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
+        { key = "x", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
+        { key = "n", mods="SHIFT|CTRL",     action="ToggleFullScreen" },
+        { key = "v", mods="SHIFT|CTRL",     action="Paste"},
+        { key = "c", mods="SHIFT|CTRL",     action="Copy"},
+    },
+    set_environment_variables = {},
+}
+
+return config
