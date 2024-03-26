4d75a2e2bd10275d991493c8235926f17ada7cdc
Ying Fang <fangying712@gmail.com>
2024-03-26 15:08:27 +0800

    Update wezterm.lua
    Signed-off-by: Ying Fang <fangying712@gmail.com>

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -57,9 +57,9 @@ local config = {
 
 	-- customize
 	use_fancy_tab_bar = false,
-	hide_tab_bar_if_only_one_tab = true,
-	window_decorations = "RESIZE",
-	show_new_tab_button_in_tab_bar = false,
+	-- hide_tab_bar_if_only_one_tab = true,
+	-- jwindow_decorations = "RESIZE",
+	-- show_new_tab_button_in_tab_bar = false,
 	window_background_opacity = 0.9,
 	macos_window_background_blur = 70,
 	text_background_opacity = 0.9,
